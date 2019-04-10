528ecf7ca093c39cf6e1dede7a66fb8df0a6e7e7
dit03 <dit@imac-4.local>
2019-04-10 15:49:03 +0900

    First Commit

--- Prime Number/ViewController.swift
+++ Prime Number/ViewController.swift
@@ -8,13 +8,55 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
+
+    @IBOutlet weak var txtInput: UITextField!
+    @IBOutlet weak var txtResult: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        txtInput.delegate = self
+        txtInput.clearButtonMode = UITextField.ViewMode.always
     }
-
-
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        view.endEditing(true)
+    }
+    
+    @IBAction func Check(_ sender: UIButton) {
+        var isPrime = true
+        if let number = Int(txtInput.text!) {
+            if number <= 0 {
+                txtResult.text = "자연수를 입력하시오"
+                return
+            }
+            if number == 1 {
+                isPrime = false
+            }
+            if (number != 1 && number != 2) {
+                for i in 2..<number {
+                    if (number % i == 0) {
+                        isPrime = false
+                    }
+                }
+            }
+            if isPrime {
+                txtResult.text = "소수입니다"
+                print("소수")
+            } else {
+                txtResult.text = "소수가 아닙니다."
+                print("소수아님")
+            }
+        } else {
+            txtResult.text = "숫자를 입력하시오"
+        }
+        txtInput.text = ""
+    }
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        txtInput.resignFirstResponder()
+        return true
+    }
+    
+    
 }
 
